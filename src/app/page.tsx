fd42e36c9372378bacf781ef3d1b26305374c883
Prabhat <pkjha187@gmail.com>
2023-07-17 22:02:14 +0530

    added: Table data fetching, table, main ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Table from "../components/table"
 
 export default function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello world!!</h1>
+    <main className="flex w-full min-h-screen items-center justify-center py-20 px-2 lg:px-24 ">
+      {/* <h1>Hello world!!</h1> */}
+      <Table/>
     </main>
   )
 }
--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,8 +1,59 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
+
+interface TableDataItem {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+  }
+}
 
 const table = () => {
+
+  const [tableData, setTableData] = useState<TableDataItem[]>([]);
+  const tableHeaderItems = ["id", "title", "price", "category", "rating"]
+
+  const getData = async () => {
+    const res = await fetch(`https://fakestoreapi.com/products`);
+    const data = await res.json()
+    console.log(data)
+    setTableData(data);
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+
   return (
-    <div>table</div>
+    <div className='w-full flex items-start justify-start flex-col  gap-2 overflow-x-auto ' >
+      <input type='text' placeholder='Search table...' className=' border-2 border-slate-500 rounded px-4 py-2 text-base outline-none text-gray-600 ' />
+      <table className='w-full border-collapse overflow-x-auto ' >
+        <thead>
+        <tr>
+          {tableHeaderItems.map((item) => (
+            <th key={item} className='capitalize text-left p-2 border-slate-400 border text-base font-bold text-gray-700' >{item} </th>
+          ))}
+        </tr>
+        </thead>
+        <tbody>
+          {tableData?.map((item) => (
+            <tr key={item.id} className='capitalize text-left p-2 border-slate-400 border even:bg-gray-200' >
+              <td className='capitalize text-left p-2 border-slate-400 border text-base text-gray-600 ' > {item.id} </td>
+              <td className='capitalize text-left p-2 border-slate-400 border text-base text-gray-600'> {item.title} </td>
+              <td className='capitalize text-left p-2 border-slate-400 border text-base text-gray-600'> ${item.price} </td>
+              <td className='capitalize text-left p-2 border-slate-400 border text-base text-gray-600'> {item.category} </td>
+              <td className='capitalize text-left p-2 border-slate-400 border text-base text-gray-600'> {item.rating.rate} </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
