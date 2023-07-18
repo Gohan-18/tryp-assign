fba27074b7ca1d1335182e53c76da26cbe3e9433
Prabhat <pkjha187@gmail.com>
2023-07-18 23:06:37 +0530

    added: basic sort by id,price,rate to the table

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -14,10 +14,20 @@ interface TableDataItem {
   };
 }
 
+interface SortbyList {
+  id: boolean;
+  price: boolean;
+  rate: boolean;
+}
+
 const table = () => {
   const [tableData, setTableData] = useState<TableDataItem[]>([]);
   const [searchedTerm, setSearchedTerm] = useState<string>("");
-  const tableHeaderItems = ["id", "title", "price", "category", "rating"];
+  const [sortbyList, setSortbyList] = useState<SortbyList>({
+    id: true,
+    price: false,
+    rate: false,
+  });
 
   const getData = async () => {
     const res = await fetch(`https://fakestoreapi.com/products`);
@@ -26,14 +36,50 @@ const table = () => {
     setTableData(data);
   };
 
+  const compare = (a: TableDataItem, b: TableDataItem) => {
+    let numA: number = 0;
+    let numB: number = 0;
+
+    if (sortbyList.id === true) {
+      numA = a.id;
+      numB = b.id;
+    } else if (sortbyList.price === true) {
+      numA = a.price;
+      numB = b.price;
+    } else if (sortbyList.rate === true) {
+      numA = a.rating.rate;
+      numB = b.rating.rate;
+    }
+
+    if (numA < numB) {
+      return -1;
+    } else if (numA > numB) {
+      return 1;
+    } else {
+      return 0;
+    }
+  };
+
   const filteredData =
     searchedTerm === ""
       ? tableData
       : tableData?.filter(
           (item) =>
-            item.category.toLowerCase().includes(searchedTerm) ||
-            item.title.toLowerCase().includes(searchedTerm)
+            item.category.toLowerCase().includes(searchedTerm.toLowerCase()) ||
+            item.title.toLowerCase().includes(searchedTerm.toLowerCase())
         );
+
+  if (sortbyList.id === true) {
+    filteredData?.sort(compare);
+  }
+
+  if (sortbyList.price === true) {
+    filteredData?.sort(compare);
+  }
+
+  if (sortbyList.rate === true) {
+    filteredData?.sort(compare);
+  }
 
   useEffect(() => {
     getData();
@@ -45,24 +91,71 @@ const table = () => {
 
   return (
     <div className="w-full flex items-start justify-start flex-col  gap-2 overflow-x-auto ">
-      <input
-        value={searchedTerm}
-        onChange={(e) => setSearchedTerm(e.target.value)}
-        type="text"
-        placeholder="Search table..."
-        className=" border-2 border-slate-500 rounded px-4 py-2 text-base outline-none text-gray-600 "
-      />
+      <div className="w-full flex items-center justify-start gap-2 flex-col md:flex-row ">
+        <input
+          value={searchedTerm}
+          onChange={(e) => setSearchedTerm(e.target.value)}
+          type="text"
+          placeholder="Search table..."
+          className=" border-2 max-w-xs border-slate-500 rounded px-4 py-2 text-base outline-none text-gray-600 "
+        />
+      </div>
+
       <table className="w-full border-collapse overflow-x-auto ">
         <thead>
           <tr>
-            {tableHeaderItems.map((item) => (
-              <th
-                key={item}
-                className="capitalize text-left p-2 border-slate-400 border text-base font-bold text-gray-700"
-              >
-                {item}{" "}
-              </th>
-            ))}
+            <th
+              onClick={() => {
+                setSortbyList({
+                  id: true,
+                  price: false,
+                  rate: false,
+                });
+              }}
+              className={`capitalize cursor-pointer text-left p-2 border-slate-400 border text-base font-bold text-gray-700 ${
+                sortbyList.id === true && "bg-green-200"
+              } `}
+            >
+              Id
+            </th>
+            <th
+              className={`capitalize text-left p-2 border-slate-400 border text-base font-bold text-gray-700 `}
+            >
+              Title
+            </th>
+            <th
+              onClick={() => {
+                setSortbyList({
+                  id: false,
+                  price: true,
+                  rate: false,
+                });
+              }}
+              className={`capitalize cursor-pointer text-left p-2 border-slate-400 border text-base font-bold text-gray-700  ${
+                sortbyList.price === true && "bg-green-200"
+              } `}
+            >
+              Price
+            </th>
+            <th
+              className={`capitalize text-left p-2 border-slate-400 border text-base font-bold text-gray-700 `}
+            >
+              Category
+            </th>
+            <th
+              onClick={() => {
+                setSortbyList({
+                  id: false,
+                  price: false,
+                  rate: true,
+                });
+              }}
+              className={`capitalize cursor-pointer text-left p-2 border-slate-400 border text-base font-bold text-gray-700  ${
+                sortbyList.rate === true && "bg-green-200"
+              }  `}
+            >
+              Rating
+            </th>
           </tr>
         </thead>
         <tbody>
